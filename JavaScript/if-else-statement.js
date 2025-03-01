3f2868dd625bcec44e53ed7c065b27273c836622
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:36:17 -1200

    if else statement

---
+++ JavaScript/if-else-statement.js
@@ -0,0 +1,13 @@
+// ======================
+//   if-else-statement
+// ======================
+
+let myName = "Hussain";
+// let gender = "male";
+let gender = "female";
+
+if (gender === "male") {
+  console.log("Hello Mr." + myName);
+} else {
+  console.log("Hello Miss." + myName);
+}
