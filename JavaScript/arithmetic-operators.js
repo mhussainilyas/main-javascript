b45632b4ac29c8b14e9542228521c5bc180f2cf1
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:10:38 -1200

    arithmetic operators

---
+++ JavaScript/arithmetic-operators.js
@@ -0,0 +1,38 @@
+// ======================
+//  Arithmetic Operators
+// ======================
+
+let a = 10;
+let b = 20;
+
+// 1. addition (+)
+// let c = a + b;
+// console.log(c);
+
+// 2. subtraction (-)
+// let c = b - a;
+// console.log(c);
+
+// 3. multipication (*)
+// let c = a * b;
+// console.log(c);
+
+// 4. exponentation (**)
+// let c = b ** 3;
+// console.log(c);
+
+// 5. division (/)
+// let c = b / a;
+// console.log(c);
+
+// 6. modulus(remainder) (%)
+// let c = b % a;
+// console.log(c);
+
+// 7. increament (++)
+// let c = ++a;
+// console.log(c);
+
+// 8. decreament (--)
+let c = --a;
+console.log(c);
