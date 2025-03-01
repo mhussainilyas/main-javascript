c2edc95f3a7c83d647a416cedce426a0932cb4ca
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:11:00 -1200

    comaprison operators

---
+++ JavaScript/comparison-operators.js
@@ -0,0 +1,43 @@
+// ==========================
+//   Comaprison Operators
+// ==========================
+
+// 1. Equal to (==)
+// let a = 10;
+// let b = 20;
+// console.log(a == b);
+
+// 2. Not Equal to (!=)
+// let a = 10;
+// let b = 20;
+// console.log(a != b);
+
+// 3. Equal value and eaual type (===)
+// let a = 10;
+// let b = "10";
+// console.log(a === b);
+
+// 4. Not equal value or not eaual type (!==)
+// let a = 10;
+// let b = "10";
+// console.log(a !== b);
+
+// 5. greater then (>)
+// let a = 10;
+// let b = 20;
+// console.log(a > b);
+
+// 6. less then (<)
+// let a = 10;
+// let b = 20;
+// console.log(a < b);
+
+// 7. greater then or equal to (>=)
+// let a = 10;
+// let b = 10;
+// console.log(a >= b);
+
+// 8. less then or equal to (<=)
+let a = 10;
+let b = 20;
+console.log(a <= b);
