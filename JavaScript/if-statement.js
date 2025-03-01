4f0fd58d6b3f30af1e4cf9ec1cb5d8aece3d2c8e
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:36:23 -1200

    if statement

---
+++ JavaScript/if-statement.js
@@ -0,0 +1,22 @@
+// =================
+//   if-statement
+// =================
+
+let a = 10;
+// let b = 20;
+
+if (a > b) {
+  console.log("a is greater then b");
+}
+
+if (a < b) {
+  console.log("a is less then b");
+}
+
+if (true) {
+    console.log("this is true");
+}
+
+if (false) {
+    console.log("this is true");
+}
