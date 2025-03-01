6c49a55d809ceb7b58b36470551dbb0c3eb7b22b
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:36:06 -1200

    logical operators

---
+++ JavaScript/logical-operators.js
@@ -0,0 +1,20 @@
+// ======================
+//   Logical Operators
+// ======================
+
+let age = 19;
+
+// 1. Logical AND (&&)
+if (age >= 11 && age <= 18) {
+  console.log("You are eligible.");
+}
+
+// 2. Logical OR (||)
+if (age >= 11 || age <= 18) {
+  console.log("You are eligible.");
+}
+
+// 3. Logical NOT (!)
+if (!(age != 19)) {
+  console.log("Your age is 19.");
+}
