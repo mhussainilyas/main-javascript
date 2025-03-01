b76501f299311348ed987d6badc29ea897bf5b54
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:11:07 -1200

    data types

---
+++ JavaScript/data-types.js
@@ -0,0 +1,40 @@
+// ==============
+//   Data Types
+// ==============
+
+// 1. String
+// let my_data = "Hussain";
+
+// 2. Number
+// let my_data = 19;
+
+// 3. null
+// let my_data = null;
+
+// 4. undefined
+// let my_data;
+// let my_data = undefined;
+
+// 5. Boolean
+// let my_data = true;
+// let my_data = false;
+
+// 6. BigInt
+// let my_data = 10n;
+// let my_data = BigInt(10);
+
+// 7. symbol
+// let my_data = Symbol("symbol data");
+// console.log(my_data);
+// console.log(my_data.description);
+
+// 8. Array
+// let my_data = [1, 2, 3, 4, 5];
+// let my_data = ["Hussain", "Suleman", "Zaryab"];
+
+// 9. Object
+let my_data = { a: 1, b: 2, c: 3, d: 4 };
+// let my_data = { a: "Hussain", b: "Suleman", c: "Zaryab" };
+
+console.log(my_data);
+console.log(typeof my_data);
