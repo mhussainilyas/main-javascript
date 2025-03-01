42ea2b6fd06f2d3424052227576f085301da27e7
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 17:11:14 -1200

    console

---
+++ JavaScript/google-chrome-console.js
@@ -0,0 +1,13 @@
+// ===================
+//   Chrome Console
+// ===================
+
+// console.log("Hussain");
+// console.table([1, 2, 3, 4, 5]);
+// console.error("Something went wrong by Hussain Ilyas");
+// console.warn("This is just warning.");
+// console.clear();
+
+console.time("time");
+console.log("Muhammad Hussain Ilyas");
+console.timeEnd("time");
