a4d52cdc5c87166c32ccac2826bb2d1ac810c429
mhussainilyas <mhussainilyas7@gmail.com>
2025-02-28 13:33:05 -1200

    varaibles

---
+++ JavaScript/variables.js
@@ -0,0 +1,29 @@
+// =============
+//   Variables
+// =============
+
+// Creating Variables:
+// -------------------
+
+// var x = "Muhammad Hussain";
+// var x = 'Muhammad Hussain';
+// document.write(x);
+
+// Naming Variables:
+// -----------------
+
+// var first_name = "Dream Coding";
+// var firstName = "Dream Coding";
+// var firstName99 = "Dream Coding";
+// document.write(firstName99);
+
+// Declaring Variables:
+// --------------------
+
+var myName, age, department;
+myName = "Hussain <br>";
+age = 19;
+department = '<br> BSCS';
+document.write(myName);
+document.write(age);
+document.write(department);
