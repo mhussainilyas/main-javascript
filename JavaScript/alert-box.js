4bf63567206c2924a0925f4372f4ad690633ddee
mhussainilyas <mhussainilyas7@gmail.com>
2025-03-01 22:38:29 -1200

    alert box

---
+++ JavaScript/alert-box.js
@@ -0,0 +1,12 @@
+// alert("Muhammad Hussain");
+// alert(10 + 5);
+
+let age = 30;
+
+// if (age <= 20) {
+//   alert("Teenager");
+// } else {
+//   alert("Young");
+// }
+
+alert("My age is: " + age);
