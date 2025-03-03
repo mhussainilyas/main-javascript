d028657deecd8221de2b436c05b1480de4b15f6f
mhussainilyas <mhussainilyas7@gmail.com>
2025-03-02 13:30:12 -1200

    confirm box

---
+++ JavaScript/confirm-box.js
@@ -0,0 +1,9 @@
+// confirm("Do you like our website?");
+let a = confirm("Do you like our website?");
+document.write(a);
+document.write("<br>");
+if (a) {
+  document.write("Thanks...");
+} else {
+  document.write("Sorry...");
+}
